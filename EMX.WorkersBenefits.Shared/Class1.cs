7d1e1f28494c7fd36984002f71cf769a6dff7158
EMXWorkersBenefits <30990397+EMXWorkersBenefits@users.noreply.github.com>
2017-08-14 13:52:38 +0300

    add new files

---
+++ EMX.WorkersBenefits.Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMX.WorkersBenefits.Shared
+{
+    public class Class1
+    {
+    }
+}
